dbef63dd88c75c74ce020ff7606dc24cb0cd69c8
Yaroslav Onuchak <onuchak.j@gmail.com>
2020-03-24 23:50:51 +0200

    first comit

---
+++ main.js
@@ -0,0 +1,38 @@
+document.querySelector('.btn').addEventListener('click', function () {
+  let serch = document.serchForm.serchVal.value;
+  let htmlReques = new XMLHttpRequest();
+
+  htmlReques.open('GET', `https://api.itbook.store/1.0/search/${serch}`)
+
+  document.serchForm.reset();
+  document.querySelector('.result').innerHTML = ''
+
+  htmlReques.onload = () => {
+    if (htmlReques.readyState == 4 && htmlReques.status == 200) {
+      let data = JSON.parse(htmlReques.responseText);
+      // console.log(data)
+      // console.log(htmlReques.readyState)
+      // console.log(htmlReques.status)
+
+      if (data.total > 0) {
+        let foundBooks = data.books.length;
+        let allBooks = data.total;
+        let arrBooks = data.books;
+
+        document.querySelector('.result').innerHTML = `<p> first ${foundBooks} books from ${allBooks}</p>`
+        for (let items of arrBooks) {
+          document.querySelector('.result').innerHTML += `<a href="${items.url}" > ${items.title}</a><br>`
+        }
+      } else {
+        document.querySelector('.result').innerText = 'no result'
+      }
+
+    } else {
+      document.querySelector('.result').innerText = 'not today'
+
+    }
+  }
+  htmlReques.send()
+
+
+})
