0386d44641e4d3b8f9f5345a68489c915d977824
Yaroslav Onuchak <onuchak.j@gmail.com>
2020-03-25 15:03:47 +0200

    change api link,

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ document.querySelector('.btn').addEventListener('click', function () {
   let serch = document.serchForm.serchVal.value;
   let htmlReques = new XMLHttpRequest();
 
-  htmlReques.open('GET', `https://api.itbook.store/1.0/search/${serch}`)
+  // htmlReques.open('GET', `https://api.itbook.store/1.0/search/${serch}`)
+  htmlReques.open('GET', `https://it-ebooks-api.info/v1/search/${serch}`)
 
   document.serchForm.reset();
   document.querySelector('.result').innerHTML = ''
 
   htmlReques.onload = () => {
     if (htmlReques.readyState == 4 && htmlReques.status == 200) {
-      let data = JSON.parse(htmlReques.responseText);
-      // console.log(data)
-      // console.log(htmlReques.readyState)
-      // console.log(htmlReques.status)
+      let data = JSON.parse(htmlReques.responseText.toLowerCase());
+      console.log(data)
+      console.log(data.total)
+      console.log(htmlReques.readyState)
+      console.log(htmlReques.status)
 
       if (data.total > 0) {
         let foundBooks = data.books.length;
