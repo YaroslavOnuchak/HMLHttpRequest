2de20c1bffceb4ef26732998f10fc86143b48368
Yaroslav Onuchak <onuchak.j@gmail.com>
2020-03-25 22:33:30 +0200

    minimiz

--- main.js
+++ main.js
@@ -1,40 +1,25 @@
-document.querySelector('.btn').addEventListener('click', function () {
-  let serch = document.serchForm.serchVal.value;
-  let htmlReques = new XMLHttpRequest();
-
-  // htmlReques.open('GET', `https://api.itbook.store/1.0/search/${serch}`)
-  htmlReques.open('GET', `https://it-ebooks-api.info/v1/search/${serch}`)
-
-  document.serchForm.reset();
-  document.querySelector('.result').innerHTML = ''
-
-  htmlReques.onload = () => {
-    if (htmlReques.readyState == 4 && htmlReques.status == 200) {
-      let data = JSON.parse(htmlReques.responseText.toLowerCase());
-      console.log(data)
-      console.log(data.total)
-      console.log(htmlReques.readyState)
-      console.log(htmlReques.status)
-
-      if (data.total > 0) {
-        let foundBooks = data.books.length;
-        let allBooks = data.total;
-        let arrBooks = data.books;
-        document.querySelector('.result').innerHTML = `<p> first ${foundBooks} books from ${allBooks}</p>`
-
-        for (let items of arrBooks) {
-          document.querySelector('.result').innerHTML += `<a href="https://itbook.store/books/${items.isbn}" title="${items.description}"> ${items.title}</a><br>`
+window.onload = () => {
+  document.querySelector('.btn').addEventListener('click', function () {
+    let serch = document.serchForm.serchVal.value;
+    let htmlReques = new XMLHttpRequest();
+    htmlReques.open('GET', `https://it-ebooks-api.info/v1/search/${serch}`)
+    document.serchForm.reset();
+    document.querySelector('.result').innerHTML = '';
+    htmlReques.onload = () => {
+      if (htmlReques.readyState == 4 && htmlReques.status == 200) {
+        let data = JSON.parse(htmlReques.responseText.toLowerCase());
+        if (data.total > 0) {
+          document.querySelector('.result').innerHTML = `<p>first ${data.books.length} books from ${data.total}</p>`
+          for (let items of data.books) {
+            document.querySelector('.result').innerHTML += `<a href="https://itbook.store/books/${items.isbn}" title="${items.description}"> ${items.title}</a><br>`
+          }
+        } else {
+          document.querySelector('.result').innerText = 'no result'
         }
       } else {
-        document.querySelector('.result').innerText = 'no result'
+        document.querySelector('.result').innerText = 'not today'
       }
-
-    } else {
-      document.querySelector('.result').innerText = 'not today'
-
     }
-  }
-  htmlReques.send()
-
-
-})
+    htmlReques.send()
+  })
+};
